ebf32ac60bd5f4ce632fb82ab4ee5347c9581720
Le_Van_Huy_Hoang <hoanglvhse170333@fpt.edu.vn>
2025-01-20 17:11:12 +0700

    upload video

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const multer = require('multer')
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
+const express = require('express');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 const {
   addComment,
   addNewPost,
@@ -13,20 +13,20 @@ const {
   getPostById,
   getUserPost,
   likePost
-} = require('../controllers/post.controller.js')
-const isAuthenticated = require('../middlewares/isAuthenticated.js')
+} = require('../controllers/post.controller.js');
+const isAuthenticated = require('../middlewares/isAuthenticated.js');
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/addpost').post(isAuthenticated, upload.array('media'), addNewPost)
-router.route('/all').get(isAuthenticated, getAllPost)
-router.route('/userpost/all').get(isAuthenticated, getUserPost)
-router.route('/:id/like').put(isAuthenticated, likePost)
-router.route('/:id/dislike').put(isAuthenticated, dislikePost)
-router.route('/:id/comment').post(isAuthenticated, addComment)
-router.route('/:id/comment/all').post(isAuthenticated, getCommentsOfPost)
-router.route('/:id/getpostbyid').get(isAuthenticated, getPostById)
-router.route('/:id').delete(isAuthenticated, deletePost)
-router.route('/:id/bookmark').get(isAuthenticated, bookmarkPost)
+router.route('/addpost').post(isAuthenticated, upload.array('media'), addNewPost);
+router.route('/all').get(isAuthenticated, getAllPost);
+router.route('/userpost/all').get(isAuthenticated, getUserPost);
+router.route('/:id/like').put(isAuthenticated, likePost);
+router.route('/:id/dislike').put(isAuthenticated, dislikePost);
+router.route('/:id/comment').post(isAuthenticated, addComment);
+router.route('/:id/comment/all').post(isAuthenticated, getCommentsOfPost);
+router.route('/:id/getpostbyid').get(isAuthenticated, getPostById);
+router.route('/:id').delete(isAuthenticated, deletePost);
+router.route('/:id/bookmark').get(isAuthenticated, bookmarkPost);
 
-module.exports = router
+module.exports = router;
