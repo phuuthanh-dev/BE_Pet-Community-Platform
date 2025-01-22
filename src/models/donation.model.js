c03918cc60fc26b842b0ece465409432cc7e7c05
Phung Huu Thanh <thanhphse170345@fpt.edu.vn>
2025-01-22 22:58:25 +0700

    donation api

--- src/models/donation.model.js
+++ src/models/donation.model.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose')
+const { TRANSACTION_STATUS } = require('../constants/enums')
 
 const donationSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     campaign: { type: mongoose.Schema.Types.ObjectId, ref: 'Campaign', required: true },
     amount: { type: Number, required: true },
-    message: { type: String, required: true }
+    description: { type: String, required: true },
+    code: { type: String, unique: true, required: true },
+    paymentUrl: { type: String, default: '' },
+    counterAccountName: { type: String, default: '' },
+    counterAccountNumber: { type: String, default: '' },
+    transactionDateTime: { type: String, default: '' },
+    status: {
+      type: String,
+      enum: Object.values(TRANSACTION_STATUS),
+      default: TRANSACTION_STATUS.PENDING,
+      required: true
+    }
   },
   { timestamps: true }
 )
